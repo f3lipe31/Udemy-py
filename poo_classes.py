487385ec8e2a4167bc61baa86e94d472ece778b2
f3lipe31 <lipecunha2005@gmail.com>
2026-04-05 12:21:46 -0300

    Classes em poo.py

---
+++ poo_classes.py
@@ -0,0 +1,43 @@
+"""
+Classes em poo , sao modelos dos objetos do mundo real sendo representados
+computacionamlente
+
+imagine que vc queira fazer um sistema para automatizar o controle das lampadas da sua casa
+
+
+classes podems conter :
+    atributos - representam as caracteristicas do objeto, ou seja, pelos atribustos conseguimos
+    representar computacionalmente os estados do objeto.No caso da lampada todas as caracteriscyacs
+    woltz,cor,força,tipo
+
+    Metodos(funções) - representam o comportamento, as ações que ele pode realizar,no caso da lampada
+    um comportamento comum que iriamos querer representar no sistema é ela ligar e desligar 
+
+
+Em python para definir uma classe se utiliza a palavra - class
+
+Obs:utiliza a palavra pass, quando tem um bloco de codigo que ainda nao
+esta implementada
+
+Obs:quando se nomeia nossas classes, utiliza por convenção o nome em inicial 
+maisculo, se o nome for composto se utiliza as duas iniciais maisculas,tudo junto
+
+Obs:quando estamos planejando um software e definimos quais classes teremos que ter no sistema, chamamos
+os objetos que serao mapeados para classes de entidades
+    
+
+"""
+class Lampada:
+    pass
+
+class ContaCorrente:
+    pass
+
+class Produto:
+    pass
+
+class Usuario:
+    pass
+
+valor = int ("42") #cast
+print (help(int))
