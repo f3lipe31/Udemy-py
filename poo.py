41f63a842867924f8a8fe9eb91f264d7f2a125e4
f3lipe31 <lipecunha2005@gmail.com>
2026-04-05 11:57:50 -0300

    Seção 15 Poo em py

---
+++ poo.py
@@ -0,0 +1,24 @@
+"""
+POO- é um paradigma de programação que utiliza mapeamento 
+de objetos do mundo real para modelos de computadores.
+
+- paradigma de progamação é a forma utilizada para 
+pensar /desenvolver sistemas
+"""
+
+numero = 10
+print (numero)
+print(type(numero))
+
+
+nome = "Geek"
+print (nome)
+print(type(nome))
+
+
+class Produtos:
+    pass
+
+ps4 = Produtos()
+print (ps4)
+print(type(ps4))
